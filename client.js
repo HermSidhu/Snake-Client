9d06a97148a37690e5df64f9d02669a62a0cc671
HermSidhu <h-sidhu@hotmail.com>
2019-10-23 18:56:42 +0000

    enables exiting game with ctrl-c

--- client.js
+++ client.js
@@ -11,6 +11,12 @@ const connect = function() {
     console.log('Successfully connected to the game server');
   });
   conn.write("Name: SNK");
+  // setInterval(() => {
+  //   conn.write("Move: up");
+  // }, 100);
+  // setInterval(() => {
+  //   conn.write("Move: right")
+  // }, 100)
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   return conn;
--- play.js
+++ play.js
@@ -4,3 +4,18 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+const handleUserInput = function() {
+  const stdin = process.stdin.on('data', (key) => {
+    if (key === '\u0003') 
+      process.exit(console.log("\nThanks for slithering!"));
+})};
+handleUserInput()
+setupInput();
